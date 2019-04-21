9a479b5e28939349980a756cfe48a9a29714c821
Bruno Frank <bfscordeiro@gmail.com>
2019-04-21 15:21:19 -0300

    Add add methods to match

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
-::Refinery::Application.routes.draw do
+Refinery::Core::Engine.routes.draw do
   scope(:path => 'refinery', :as => 'admin', :module => 'admin') do
-    match 'dashboard',
+    get 'dashboard',
           :to => 'dashboard#index',
-          :as => :dashboard
+          :as => :dashboard,
+          :via => [:get, :post, :put]
 
     match 'disable_upgrade_message',
           :to => 'dashboard#disable_upgrade_message',
-          :as => :disable_upgrade_message
+          :as => :disable_upgrade_message,
+          :via => [:get, :post, :put]
   end
 end
